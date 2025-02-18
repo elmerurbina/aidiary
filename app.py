ec2ff840e70ab5fd340d07d994fe716b20c5637b
foxomax <holmanrugama2006@gmail.com>
2025-02-17 23:02:31 -0600

    frontend improvements

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from routes.routes import setup_routes
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY', '4921d111085d8c5ca331af4c50a3cc0b11e0db88ea1befb6a257142b83589e49')
+app.secret_key = os.getenv('SECRET_KEY')
 
 # Configurar las rutas
 setup_routes(app)
--- models/models.py
+++ models/models.py
@@ -10,11 +10,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Database connection details
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", 5432)
-DB_NAME = os.getenv("DB_NAME", "aidiary")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "jesucristo12")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # Initialize the connection pool
 try:
